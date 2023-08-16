c4c0d75bb12b7e22b8c3f552cb4eb27d6085af81
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-16 22:09:36 +0000

    removed trailing whitespace

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     s_d = sorted(a_dictionary)
     for i in s_d:
-        print("{:s}: {}".format(i, a_dictionary[i])) 
+        print("{:s}: {}".format(i, a_dictionary[i]))
