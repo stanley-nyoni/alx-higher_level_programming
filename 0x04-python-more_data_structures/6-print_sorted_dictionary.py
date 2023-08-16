b79b533f8af159e2c167c45ed7723e2d6dd964b9
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-16 22:06:51 +0000

    sorted dic - fixed a small bug

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_dictionary = sorted(a_dictionary)
-    for i in sorted_dictionary:
-        print("{:s}: {}".format(i, a_dctionary[i])) 
+    s_d = sorted(a_dictionary)
+    for i in s_d:
+        print("{:s}: {}".format(i, a_dictionary[i])) 
