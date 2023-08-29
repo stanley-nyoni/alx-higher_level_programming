ffcfcf41df1abebca2ab490f56c28ed4e21b939a
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-29 20:22:32 +0000

    1 - square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Creates a square with size """
+
+
+class Square:
+    """ Defines a square """
+    def __init__(self, size):
+        """ Initialize a square """
+        self.size = size
