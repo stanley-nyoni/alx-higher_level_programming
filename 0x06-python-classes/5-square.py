884b74b0e4d36dfb1b2f8bf03ba601f5470d0b0a
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-30 21:02:31 +0000

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" Defines a square class."""
+
+
+class Square:
+    """ Initialise the square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
