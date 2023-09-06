9886f7625f1b662dbe0ad0452d2d53c0cf3b8eb9
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-09-06 19:05:22 +0000

    rect to square - corrections - 2

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -74,7 +74,7 @@ class Rectangle:
             return rect_2
         else:
             return rect_1
-
-        @classmethod
-        def square(cls, size=0):
-            return cls(size, size)
+    
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
