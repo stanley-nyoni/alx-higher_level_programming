3cc0ef938664652806aff738226237bee82fff20
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-09-11 20:27:50 +0000

    is kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""""Kind of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Return true if the object in a instance of class"""
+
+    return isinstance(obj, a_class)
