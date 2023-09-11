846af0b577c13a7daa684acfae8a23decaea6a40
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-09-11 18:48:03 +0000

    print attributes and methods available for an obj

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""""" Prints the available attributes and methods for an obj."""
+
+
+def lookup(obj):
+    """"" Returns the list of available attributes and methods"""
+    return dir(obj)
