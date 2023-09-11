ece9565459aa284ff8328f0697712c7fff27dac9
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-09-11 20:21:28 +0000

    is same class - bug fix(1)

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,8 @@
 def is_same_class(obj, a_class):
     """" Returns true if obj is an instance of a class"""
 
-    return type(obj) == a_class
+    obj_type = type(obj)
+    if obj_type != a_class:
+        return False
+
+    return True
