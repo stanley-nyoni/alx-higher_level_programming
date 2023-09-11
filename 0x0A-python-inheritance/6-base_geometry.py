7148e2bdeecb683d0e4111e9c97e6848c7fdfe10
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-09-11 20:48:24 +0000

    improve bas geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Defines a BaseGeometry class """
+
+
+class BaseGeometry:
+    """Initialize a BaseGeometry class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
