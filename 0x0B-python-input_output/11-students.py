1438da22bacaa981787a1cf9ab591c16696267c1
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-09-13 20:08:43 +0000

    reload students

---
+++ 0x0B-python-input_output/11-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""10-student - Defines a class Student"""
+
+
+class Student:
+    """Initialising a Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            my_dict = {}
+            for x in attrs:
+                if hasattr(self, x):
+                    my_dict[x] = getattr(self, x)
+            return my_dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
