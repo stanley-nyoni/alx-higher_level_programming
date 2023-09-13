30e084f61ee235ca5abc9df2ada4e419b69978f6
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-09-13 20:58:41 +0000

    add new attribute to obj - bug fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,12 @@
 
 
 def add_attribute(obj, attr_name, attr_value):
+    """Adding new attribute to obj
+    Args:
+        obj: object
+        attr_name: attribute name
+        attr_value: attribute value
+    """
     if hasattr(obj, '__dict__'):
         setattr(obj, attr_name, attr_value)
     else:
