37e3b9f4190b4254207a459c3657b3b2705abe8a
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-09-22 20:12:40 +0000

    base class updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,6 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns a json string format of a list of dicts"""
@@ -25,7 +24,9 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
 
-        if (type(list_dictionaries) != list or not all(type(x) == dict for x in list_dictionaries)):
+        list_type = type(list_dictionaries)
+        if list_type != list or not \
+                all(isinstance(x, dict) for x in list_dictionaries):
             raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
@@ -33,10 +34,11 @@ class Base:
     def save_to_file(cls, list_objs):
         """Writes the json string format to json file"""
 
-        if list_objs is None or  list_objs == []:
+        if list_objs is None or list_objs == []:
             json_data = "[]"
         else:
-            json_data = cls.to_json_string([x.to_dictionary() for x in list_objs])
+            json_data = \
+                cls.to_json_string([x.to_dictionary() for x in list_objs])
 
         filename = cls.__name__ + ".json"
         with open(filename, "w") as f:
