46dde2c2747509023f738df3d5e326893dc871ad
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-11-19 23:28:18 +0200

    models state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+    Module: model_state.py
+    contains the class definition of a State
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+# connection_string = "mysql+mysqldb://"
+# engine = create_engine()
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
