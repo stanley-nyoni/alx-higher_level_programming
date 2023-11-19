b2b06188462547cae0cb494f34ced7e6d869e935
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-11-19 16:40:25 +0200

    filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+    Module: 2-my_filter_states
+    Lists all values in the states table of \
+    hbtn_0e_0_usa where name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    my_cursor = db.cursor()
+    my_cursor.execute("SELECT * FROM states WHERE name LIKE \
+                      BINARY %s ORDER BY id", (state_name,))
+
+    states = my_cursor.fetchall()
+    for state in states:
+        print(state)
+
+    my_cursor.close()
+    db.close()
