f31c6145e17a8d3c2e1004b2f605581171fda92c
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-11-24 21:08:27 +0200

    search for a state in a database

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""""
+    Module: 10-model_state_my_get
+    Searches for a state with the same name as arg
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    u_name = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(u_name, pwd, db), pool_pre_ping=True)
+
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+    if state:
+        print(state.id)
+    else:
+        print("Error 404: State Not found")
