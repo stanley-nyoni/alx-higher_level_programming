65acae6c728763ec87aa842208b105b1bd599e00
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-11-24 20:32:01 +0200

    list first state in database

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -29,6 +29,6 @@ if __name__ == "__main__":
     state = session.query(State).order_by(State.id).first()
 
     if state is None:
-        print()
+        print("Nothing")
     else:
         print("{}: {}".format(state.id, state.name))
