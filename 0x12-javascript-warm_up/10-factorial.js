50d1297a6ea648e05b1db306aa559ba0c585051e
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-02-12 19:57:56 +0200

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
