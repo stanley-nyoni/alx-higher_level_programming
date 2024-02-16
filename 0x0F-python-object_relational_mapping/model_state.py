f52097c7238db594bef3ce129196ef862db930bd
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-02-16 13:22:37 +0200

    model state - orm

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class"""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
