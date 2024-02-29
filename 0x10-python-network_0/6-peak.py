6d217e1ab9b1df720a48e29d0525802324a6c2f8
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-02-29 22:10:41 +0200

    bug fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,11 +2,12 @@
 
 """Find the peak in a list"""
 
+
 def find_peak(list_of_integers):
     """Find the peak in a list"""
     if list_of_integers is None or list_of_integers == []:
         return None
-    
+
     lower = 0
     upper = len(list_of_integers) - 1
     while lower < upper:
