a4239ae11400b675c4bb085e957c267aa5aa82f6
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-02-29 22:09:17 +0200

    find the peak in a list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Find the peak in a list"""
+
+def find_peak(list_of_integers):
+    """Find the peak in a list"""
+    if list_of_integers is None or list_of_integers == []:
+        return None
+    
+    lower = 0
+    upper = len(list_of_integers) - 1
+    while lower < upper:
+        mid = (lower + upper) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            lower = mid + 1
+        else:
+            upper = mid
+    return list_of_integers[lower]
